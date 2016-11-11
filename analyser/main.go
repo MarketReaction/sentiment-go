2df171890d0530808a94dadca6259a9ad3b33537
jonnyshaw89 <jon.shaw@jassoft.co.uk>
2016-11-11 09:33:38 +0000

    added mongo connection

--- analyser/main.go
+++ analyser/main.go
@@ -3,12 +3,19 @@ package main // import "github.com/MarketReaction/sentiment-go/analyser"
 import (
 	"os"
 	"fmt"
+	"gopkg.in/mgo.v2"
 )
 
 func main() {
 
-    args := os.Args[1:]
+    	args := os.Args[1:]
 
-    fmt.Println(args)
+	fmt.Println(args)
+
+	session, err := mgo.Dial(os.Getenv("MONGO_PORT_27017_TCP_ADDR") + ":" + os.Getenv("MONGO_PORT_27017_TCP_PORT"))
+        if err != nil {
+                panic(err)
+        }
+        defer session.Close()
 
 }
