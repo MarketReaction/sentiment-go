2d0b05884fbd0fa68b52f03e5f81a9d212468779
jonnyshaw89 <jon.shaw@jassoft.co.uk>
2016-11-11 09:53:57 +0000

    added story repo

--- analyser/main.go
+++ analyser/main.go
@@ -3,19 +3,19 @@ package main // import "github.com/MarketReaction/sentiment-go/analyser"
 import (
 	"os"
 	"fmt"
-	"gopkg.in/mgo.v2"
 )
 
 func main() {
 
     	args := os.Args[1:]
 
-	fmt.Println(args)
+	fmt.Println("Args: " + args)
 
-	session, err := mgo.Dial(os.Getenv("MONGO_PORT_27017_TCP_ADDR") + ":" + os.Getenv("MONGO_PORT_27017_TCP_PORT"))
-        if err != nil {
-                panic(err)
-        }
-        defer session.Close()
+	var storyId string = args[0]
 
+	fmt.Println("StoryId: " + storyId)
+
+	story := RepoFindStory(storyId)
+
+	fmt.Println("StoryTitle: " + story.title)
 }
---
+++ analyser/story.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"time"
+	"labix.org/v2/mgo/bson"
+)
+
+type Story struct {
+	Id               bson.ObjectId `json:"id" bson:"_id,omitempty"`
+	url              string        `json:"url"`
+	dateFound        time.Time        `json:"dateFound"`
+	title            string        `json:"title"`
+	body             string        `json:"body"`
+	datePublished    time.Time        `json:"datePublished"`
+
+	parentSource     string        `json:"parentSource"`
+	matchedCompanies []string        `json:"matchedCompanies"`
+	sentiment        int        `json:"sentiment"`
+}
+
+type Stories []Story
+
+
+
+//    private URL url;
+//    private Date dateFound;
+//    private String title;
+//    private String body;
+//    private Date datePublished;
+//    private NamedEntities entities;
+//
+//    private String parentSource;
+//
+//    private List<String> matchedCompanies;
+//
+//    private int sentiment;
+//
+//    private List<Metric> metrics;
