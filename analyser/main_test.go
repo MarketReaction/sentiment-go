a4ab6c320b8ff7380b530771a0377f9ce2fae090
jonnyshaw89 <jon.shaw@jassoft.co.uk>
2016-12-14 13:53:27 +0000

    insert test company

--- analyser/main_test.go
+++ analyser/main_test.go
@@ -27,7 +27,8 @@ func TestAnalyser_withNoNamedEntities_Stops(t *testing.T) {
 	var c dockertest.ContainerID
 	var apic dockertest.ContainerID
 
-	db, apic, c = setUp(story)
+	db, apic, c = setUp()
+	addTestStory(story)
 
 	defer db.Close()
 	defer apic.KillRemove()
@@ -40,11 +41,11 @@ func TestAnalyser_withNoNamedEntities_Stops(t *testing.T) {
 
 func TestAnalyser_withNamedEntities_CallsSentimentApi(t *testing.T) {
 
-	var id bson.ObjectId = bson.NewObjectId()
+	var storyId bson.ObjectId = bson.NewObjectId()
 	var matchedCompanyId bson.ObjectId = bson.NewObjectId()
 
 	story := &model.Story{
-		Id:    id,
+		Id:    storyId,
 		Title: "Test Story",
 		NamedEntities: model.NamedEntities{
 			Organisations: []model.NamedEntity{
@@ -62,22 +63,29 @@ func TestAnalyser_withNamedEntities_CallsSentimentApi(t *testing.T) {
 		MatchedCompanies: []string{matchedCompanyId.Hex()},
 	}
 
+	company := &model.Company{
+		Id:    matchedCompanyId,
+		Name:	"Test Company",
+	}
+
 	var db *mgo.Session
 	var c dockertest.ContainerID
 	var apic dockertest.ContainerID
 
-	db, apic, c = setUp(story)
+	db, apic, c = setUp()
+	addTestStory(story)
+	addTestCompany(company)
 
 	defer db.Close()
 	defer apic.KillRemove()
 	defer c.KillRemove()
 
-	os.Args = []string{"/analyse", id.Hex()}
+	os.Args = []string{"/analyse", storyId.Hex()}
 
 	main()
 }
 
-func setUp(story *model.Story) (*mgo.Session, dockertest.ContainerID, dockertest.ContainerID) {
+func setUp() (*mgo.Session, dockertest.ContainerID, dockertest.ContainerID) {
 
 	apic, ip, port, err := dockertest.SetupCustomContainer("marketreaction/sentiment-api", 8888, 10*time.Second)
 	if err != nil {
@@ -118,7 +126,15 @@ func setUp(story *model.Story) (*mgo.Session, dockertest.ContainerID, dockertest
 	os.Setenv("MONGO_PORT_27017_TCP_ADDR", "127.0.0.1")
 	os.Setenv("MONGO_PORT_27017_TCP_PORT", mongoHost[1])
 
+	return db, apic, c
+}
+
+func addTestStory(story *model.Story) {
 	session, con, err := repo.GetMongoCollection("stories")
+
+	if err != nil {
+		log.Fatalf("Failed to insert test story: %s", story.Id)
+	}
 
 	defer session.Close()
 
@@ -126,5 +142,21 @@ func setUp(story *model.Story) (*mgo.Session, dockertest.ContainerID, dockertest
 
 	con.Insert(story)
 
-	return db, apic, c
+	return
+}
+
+func addTestCompany(company *model.Company) {
+	session, con, err := repo.GetMongoCollection("companies")
+
+	if err != nil {
+		log.Fatalf("Failed to insert test company: %s", company.Id)
+	}
+
+	defer session.Close()
+
+	log.Println("Inserting story for Test")
+
+	con.Insert(company)
+
+	return
 }
